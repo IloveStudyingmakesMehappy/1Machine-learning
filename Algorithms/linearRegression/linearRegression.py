ca17027fc0a717529ae65acc80071ceddac2b647
IloveStudyingmakesMehappy <x15989366629@outlook.com>
2026-01-04 18:06:53 +0800

    first commit

---
+++ Algorithms/linearRegression/linearRegression.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-3,3,30)
+y = 2*x+ 1
+
+plt.scatter(x,y)
+#plt.show()
+
+print(x)
+print(y)
+
+x = [[i] for i in x]
+y = [[i] for i in y]
+
+print(x)
+print(y)
